01685c6671f2e836720f724ff55953b07a79afaf
David Mear <david@odegy.com>
2018-07-31 14:17:21 +0100

    Added handling for new comments and updating the page controls.

--- mockPaginate.js
+++ mockPaginate.js
@@ -26,11 +26,11 @@ var nextGrey;
 var prevGreyed = false;
 var nextGreyed = false;
 var pageLinks = [];
+var pageLinksEnd;
 var pageGrey;
 
 function setup() {
     allComments = [].slice.call(document.getElementsByClassName("comments")); // Gets a list of all the comment divs
-    totalPages = Math.ceil(allComments.length / commentsPerPage);
     prevNextThreadBox = allComments[allComments.length - 3];
     
     newCommentsMessage = document.getElementById("newcommentsmsg");
@@ -44,7 +44,9 @@ function setup() {
     }
     //There are three extra comment class elements at the end of the page, and a fourth if the "New Comments" box is there.
     
-    hideAll();
+    totalPages = Math.ceil(allComments.length / commentsPerPage);
+
+    prepareAll();
     createControls();
     updateControls();
     
@@ -63,30 +65,29 @@ var newCommentsChange = function(changes) {
                 // Refresh everything, get all the comments, redo the pages, refresh the controls, scroll back to the correct position?
                 // Need to test on the last page and on earlier pages.
                 
+                var previousComments = allComments.length;
+                
                 allComments = [].slice.call(document.getElementsByClassName("comments"));
-                totalPages = Math.ceil(allComments.length / commentsPerPage);
-                if (allComments[allComments.length - 4].id == "newcommentsmsg") {
-                    allComments.splice(allComments.length - 4, 4);
+                if (allComments[allComments.length - 5].id == "newcommentsmsg") {
+                    allComments.splice(allComments.length - 5, 5);
                 } else {
-                    allComments.splice(allComments.length - 3, 3);
+                    allComments.splice(allComments.length - 4, 4);
                 }
-                hideAll();
+                // Page controls have the class "comments", so they need to be removed from the list too.
+                // Do this more robustly, check through last few for ids?
                 
-                // Need to do a full recheck of controls here, add a page to the index list if necessary,
-                //recheck the greyed next / prev buttons, recheck the greyed page buttons.
-                // Testing to see if a dumb hard reset works:
-                indexDiv.remove(true);
-                createControls();
+                totalPages = Math.ceil(allComments.length / commentsPerPage);
+                
+                prepareNewComments(previousComments);
+                
+                createNewPageControls();
                 updateControls();
-                
-                // Half working, but getting an error:
-                // Setting comment 232 - TypeError: Cannot set property 'display' of undefined
             }
         }
     }
 }
 
-function hideAll() {
+function prepareAll() {
     var display;
     
     commentAnchors = [];
@@ -109,6 +110,26 @@ function hideAll() {
     refreshFlow();
 }
 
+function prepareNewComments(previousComments) {
+    var display;
+    
+    for (var i = previousComments; i < allComments.length; i++) {
+    
+        if (i >= currentPage * commentsPerPage && i < (currentPage + 1) * commentsPerPage) {
+            display = "";
+        } else {
+            display = "none";
+        }
+        
+        setCommentVisibility(i, display);
+        
+        if (allComments[i].previousSibling) {
+            commentAnchors[i] = allComments[i].previousSibling.name;
+        }
+
+    };
+}
+
 function setCommentVisibility(i, show) {
     try {
         // Toggle the comment visibility
@@ -126,7 +147,7 @@ function refreshFlow() {
 }
 
 function changePage(newPage) {
-    if (newPage >= 0 && newPage < totalPages) {
+    if (newPage >= 0 && newPage < totalPages && newPage != currentPage) {
     
         indexSpan.insertBefore(pageLinks[currentPage], pageGrey);
         pageGrey.remove(true);
@@ -165,9 +186,12 @@ function jumpToComment(commentAnchor) {
 }
 
 function updateControls() {
-    indexSpan.insertBefore(pageGrey, pageLinks[currentPage]);
-    pageGrey.nodeValue = currentPage + 1;
-    pageLinks[currentPage].remove(true);
+    if (indexSpan.contains(pageLinks[currentPage])) {
+        console.log("Adding greyed page.");
+        indexSpan.insertBefore(pageGrey, pageLinks[currentPage]);
+        pageGrey.nodeValue = currentPage + 1;
+        pageLinks[currentPage].remove(true);
+    }
     
     if (currentPage == 0) {
         if (!prevGreyed) {
@@ -223,24 +247,41 @@ function createControls() {
     indexSpan.appendChild(prevLink);
     indexSpan.appendChild(document.createTextNode("  |  "));
     
+    pageLinksEnd = document.createTextNode("  |  ");
+    indexSpan.appendChild(pageLinksEnd);
+    
     for (var i = 0; i < totalPages; i++) {
-        pageLinks[i] = document.createElement("a");
-        pageLinks[i].appendChild(document.createTextNode(i + 1));
-        pageLinks[i].onclick = createPageFunction(i);
-        pageLinks[i].style.cursor = "pointer";
-        indexSpan.appendChild(pageLinks[i]);
-        if (i < totalPages - 1) {
-            indexSpan.appendChild(document.createTextNode(", "));
-        }
+        createPageButton(i);
     }
     pageGrey = document.createTextNode("1");
-        
-    indexSpan.appendChild(document.createTextNode("  |  "));
+    
     indexSpan.appendChild(nextLink);
     
     indexDiv.appendChild(indexSpan);
     
+    console.log("Adding controls");
     prevNextThreadBox.parentNode.insertBefore(indexDiv, prevNextThreadBox);
+}
+
+function createNewPageControls() {
+    var i = pageLinks.length;
+    if (pageLinks.length < totalPages) {
+        indexSpan.insertBefore(document.createTextNode(", "), pageLinksEnd);
+    }
+    while (pageLinks.length < totalPages) {
+        createPageButton(i);
+    }
+}
+
+function createPageButton(i) {
+    pageLinks[i] = document.createElement("a");
+    pageLinks[i].appendChild(document.createTextNode(i + 1));
+    pageLinks[i].onclick = createPageFunction(i);
+    pageLinks[i].style.cursor = "pointer";
+    indexSpan.insertBefore(pageLinks[i], pageLinksEnd);
+    if (i < totalPages - 1) {
+        indexSpan.insertBefore(document.createTextNode(", "), pageLinksEnd);
+    }
 }
 
 function createPageFunction(i) {
@@ -250,12 +291,19 @@ function createPageFunction(i) {
 setup();
 
 //
-// Todo, for pagination:
+// Todo:
+// Bold the greyed page somehow. 21, [22], 23, 24?
 // Fix changing pages with a comment linked messing with reflow - hide linked comment arrow?
-// Is the new comments div always present?
 // Jump to the correct page when comment links are clicked
 // Refreshing when there's a comment link in the hash but you've changed pages - should it stay on the same page or jump back to the linked comment?
-// Handle new comments somehow - are there events to hook into when it appears / is clicked? -> refresh comments and redo page count
+// Test new comment handling.
+// Add handling for large numbers of pages (eg. if user changes comment count to 10). Show a few at a time + first/last? Only kick in >30?
+// Test logged out view.
+// Test classic view.
+// Test other subsites.
 // Add display of where you are in the comments?
 // Add a note to the comment form if you're not on the last page?
 // 
+// Questions
+// Is the new comments div always present?
+// Does anything else show up with the class "comments"?
